4d5e6811d030f604c1c1057350ac7ea7e4667f11
DongM91 <874901723@qq.com>
2021-11-29 18:08:26 +0800

    migration ticket repository

--- contracts/interfaces/IkakiTicket.sol
+++ contracts/interfaces/IkakiTicket.sol
@@ -1,24 +1,16 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/IERC721EnumerableUpgradeable.sol";
-
-interface IkakiTicket is IERC721EnumerableUpgradeable {
-
-
+interface IkakiTicket {
     function mint(
         address to,
-        uint tokenId,
+        uint256 tokenId,
         string calldata uri
     ) external;
 
-    // function totalMinted() external view returns (uint256);
+    function transferFrom(
+        address from, 
+        address to, 
+        uint256 tokenId) external;
 
-    // function batchTransfer(address[] memory to, uint256[] memory tokenIds) external;
-
-    // // function version() external pure returns (uint256);
-
-    // function setBaseTokenURI(string memory baseURI_) external;
-
-    // function tokensOfOwner(address _owner) external view returns (uint256[] memory);
-
+    function ownerOf(uint256 tokenId) external view returns(address);
 }
