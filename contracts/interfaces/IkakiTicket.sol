b5b8ec38db6100b07921c6d058e77064db9c971b
DongM91 <874901723@qq.com>
2021-12-03 19:29:08 +0800

    fixed bugs

--- contracts/interfaces/IkakiTicket.sol
+++ contracts/interfaces/IkakiTicket.sol
@@ -2,8 +2,6 @@ pragma solidity ^0.8.0;
 import "./IBaseERC721.sol";
 interface IkakiTicket is IBaseERC721{
     function mint(
-        address to,
-        uint256 tokenId,
-        string calldata uri
+        address to
     ) external;
 }
