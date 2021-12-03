181100513bbb61de8dbcb5f2cdfd63cdb73b333a
DongM91 <874901723@qq.com>
2021-12-03 19:29:08 +0800

    fixed bugs

--- contracts/erc721/KakiTicket.sol
+++ contracts/erc721/KakiTicket.sol
@@ -1,28 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 import "../interfaces/IkakiTicket.sol";
-contract KakiTicket is IkakiTicket{
-    address buyTicketAdd;
-
-    modifier onlyClaimAdd() {
-        require(msg.sender == buyTicketAdd, "invalid address.");
-        _;
-    }
+import "../base/BaseERC721.sol";
+contract KakiTicket is IkakiTicket, BaseERC721 {
 
     function initialize() public initializer{
         __BaseERC721_init("", "");
     }
 
     function mint(
-        address _to,
-        uint256 _tokenId
-    ) external onlyClaimAdd {
-        _mint(_to, _tokenId);
-        // super._setTokenURI(_tokenId, _uri);
-    }
-
-    function setCapClaimAdd(address claimAdd) public onlyOwner {
-        buyTicketAdd = claimAdd;
+        address _to
+    ) external override restricted {
+        //_mint(_to);
     }
 }
