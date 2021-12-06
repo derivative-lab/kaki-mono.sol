3346b63bd924681a3e18eca785a127539d5927f9
kaki007 <94599324+metacod@users.noreply.github.com>
2021-12-06 18:09:50 +0800

    feat: deploy scripts

--- utils/deployer.ts
+++ utils/deployer.ts
@@ -8,7 +8,15 @@ import {
   KakiBlindBox,
   KakiSquidGame,
   KakiSquidGame__factory,
+  Ticket,
+  Ticket__factory,
+  AddressList,
+  AddressList__factory,
+  OpenBox,
+  OpenBox__factory,
+  IERC20,
 } from '~/typechain';
+
 import { getSigner } from '~/utils/contract';
 
 export async function deployMockChainLink() {
@@ -42,20 +50,60 @@ export async function deployBlindBoxDrop() {
   return instance as KakiBlindBox;
 }
 
-export async function deploySquidGame(usdt: MockToken, chainlink: MockChainLink) {
+export async function deploySquidGame(ticket: Ticket, usdt: MockToken, chainlink: MockChainLink) {
   const signer0 = await getSigner(0);
   const factory = new KakiSquidGame__factory(signer0);
-  const args: Parameters<KakiSquidGame['initialize']> = [usdt.address, chainlink.address];
+  const args: Parameters<KakiSquidGame['initialize']> = [
+    ticket.address,
+    usdt.address,
+    chainlink.address
+  ];
   const instance = await upgrades.deployProxy(factory, args);
   console.log(`deploy squid game to: ${instance.address}`);
   await instance.deployed();
   return instance as KakiSquidGame;
 }
 
+export async function deployTicket() {
+
+  const signer0 = await getSigner(0);
+  const factory = new Ticket__factory(signer0)
+
+  const instance = await upgrades.deployProxy(factory);
+  console.log(`Ticket deployed to : ${instance.address}`)
+  return instance as Ticket;
+}
+
+
+export async function deployOpenBox(ticket: Ticket, busd: IERC20, allowList: AddressList) {
+  const signer0 = await getSigner(0);
+  const args: Parameters<OpenBox['initialize']> = [
+    ticket.address,
+    busd.address,
+    allowList.address
+  ];
+  const factory = new OpenBox__factory(signer0)
+  const instance = await upgrades.deployProxy(factory, args)
+  console.log(`OpenBox deployed to : ${instance.address}`);
+  return instance as OpenBox;
+}
+
+export async function deployAddrssList() {
+  const signer0 = await getSigner(0);
+  const factory = new AddressList__factory(signer0);
+  const instance = await factory.deploy();
+  console.log(`AddressList deployed to : ${instance.address}`);
+  return instance as AddressList;
+}
+
 export async function deployAll() {
   const usdt = await deployMockUsdt();
   const chainlink = await deployMockChainLink();
-  const game = await deploySquidGame(usdt, chainlink);
+  const ticket = await deployTicket();
 
-  return { usdt, chainlink, game };
+  const allowList = await deployAddrssList();
+  const openBox = await deployOpenBox(ticket, usdt, allowList);
+  const game = await deploySquidGame(ticket, usdt, chainlink);
+
+  return { usdt, chainlink, game, openBox, ticket, allowClaimTicket: allowList };
 }
