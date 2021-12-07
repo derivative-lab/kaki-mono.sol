57f165dc03efc1e69c727bd58a083e3ccc5f7166
DongM91 <874901723@qq.com>
2021-12-07 19:12:55 +0800

    update openBox scripts

---
+++ scripts/openBox/setLimit.ts
@@ -0,0 +1,15 @@
+import { parseEther } from 'ethers/lib/utils';
+import { squidOpenBoxContract } from '~/utils/contract';
+import {formatEther} from 'ethers/lib/utils'
+
+(async () => {
+    const openBox = await squidOpenBoxContract();
+    const tx = await openBox.setClaimLimit(50);
+    console.log(tx.hash);
+    await tx.wait();
+
+    const limit= await openBox._claimLimit();
+
+
+    console.log('_claimLimit', limit);
+})();
