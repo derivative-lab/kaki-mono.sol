2f749b50ebcd493535e9e941f1c629c6be9401dc
kaki007 <94599324+metacod@users.noreply.github.com>
2021-12-07 19:29:18 +0800

    set allow list

--- utils/contract.ts
+++ utils/contract.ts
@@ -31,7 +31,7 @@ export const mutiContractAddrs = {
     busd: '0xE70b02A5Ae129F66687256b7a5e81cC871e347D7',
     oracle: '0x8137934cF53e9ca1B4e75919dacc0364693fa69A',
     blindBoxDrop: '0x26dEa25C01f43B2cf8Da3aCe68F0DD830a4399e5',
-    squidAllowList: '0x405497ceb2E20C9E8ec5875d669ad6381BBEE79E',
+    squidAllowList: '0xad6d691fdd595D747F30f5b0C4f05d7d1E59B9F6',
     squidTicket: '0x7dc99344aA0053BC2DC16aE111e83C1315409a07',
     squidOpenBox: '0x7fc45201D0DBE2175c76995474D6394B8837C982',
   },
