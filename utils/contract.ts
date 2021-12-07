bab82beb80b7e680e87e3b975bca955b8ec260b8
kaki007 <94599324+metacod@users.noreply.github.com>
2021-12-07 11:11:58 +0800

    add squid game & openbox contract to sdk :npm :bsctest

--- utils/contract.ts
+++ utils/contract.ts
@@ -10,7 +10,10 @@ export const frontendUsedContracts = [
   'package',
   //-------  Solidity files -----
 
-  "IKakiSquidGame"
+  "IKakiSquidGame",
+  "KakiSquidGame",
+  "IOpenBox",
+  "ITicket",
 ];
 
 export const mutiContractAddrs = {
