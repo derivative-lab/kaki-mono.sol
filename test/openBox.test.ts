f62193f4d97aeb57d01c4859047328f3326f7017
DongM91 <874901723@qq.com>
2021-12-07 19:13:34 +0800

    update openbox unit tese

--- test/openBox.test.ts
+++ test/openBox.test.ts
@@ -26,10 +26,59 @@ const setup = deployments.createFixture(async () => {
 });
 
 describe('open Box', async () => {
-  it('claim', async () => {
-    const { users, openBox, usdt, ticket, allowClaimTicket } = await setup();
-
-
-
-  });
+  context("should success", async() => {
+    it('claim', async () => {
+      const { users, openBox, usdt, ticket, allowClaimTicket } = await setup();
+      for (let i = 1; i < users.length; i++) {
+        await usdt.transfer(users[i].address, parseEther('10000'));
+      }
+  
+      for (let i = 0; i < users.length; i++) {
+        await users[i].usdt.approve(openBox.address, parseEther(`1${'0'.repeat(20)}`));
+      }
+    });
+  
+    it('buyTicket', async () => {
+      const { users, openBox, usdt, ticket, allowClaimTicket } = await setup();
+      for (let i = 1; i < users.length; i++) {
+        await usdt.transfer(users[i].address, parseEther('10000'));
+      }
+  
+      for (let i = 0; i < users.length; i++) {
+        await users[i].usdt.approve(openBox.address, parseEther(`1${'0'.repeat(20)}`));
+      }
+  
+      for (let i = 0; i < users.length; i++) {
+        await users[i].openBox.buyTicket();
+        let balanceOfUser = (await ticket.balanceOf(users[i].address));
+        expect(balanceOfUser).to.equal(1);
+      }
+    })
+  
+    it('buyTicketMul', async () => {
+      const { users, openBox, usdt, ticket, allowClaimTicket } = await setup();
+      for (let i = 1; i < users.length; i++) {
+        await usdt.transfer(users[i].address, parseEther('10000'));
+      }
+  
+      for (let i = 0; i < users.length; i++) {
+        await users[i].usdt.approve(openBox.address, parseEther(`1${'0'.repeat(20)}`));
+      }
+  
+      for (let i = 0; i < users.length; i++) {
+        await users[i].openBox.buyTicketMul(2);
+        let balanceOfUser = (await ticket.balanceOf(users[i].address));
+        expect(balanceOfUser).to.equal(2);
+      }
+    })
+  })
+  
+  context("should failed", async() => {
+    it('buyTicketMul', async () => {
+      const { users, openBox, usdt, ticket, allowClaimTicket } = await setup();
+      await openBox.setAble();
+      await usdt.transfer(users[0].address, parseEther('10000'));
+      await users[1].openBox.buyTicketMul(2);
+    })
+  })
 });
