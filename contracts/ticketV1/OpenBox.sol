e1969a6408cc99b0eb7409c269734b84b337f193
DongM91 <874901723@qq.com>
2021-12-08 17:07:50 +0800

    delete buyTicketMul function in openbox & add ticket1.0 unit test

--- contracts/ticketV1/OpenBox.sol
+++ contracts/ticketV1/OpenBox.sol
@@ -63,16 +63,10 @@ contract OpenBox is IOpenBox, WithRandom, WithAdminRole {
         _claimTimeLimit[msg.sender]++;
     }
 
-    function buyTicket() public override isAble {
-        require(_busd.balanceOf(msg.sender) >= _ticketPrice, "Do not have enough BUSD.");
-        _busd.transferFrom(msg.sender, _squidGameFound, _ticketPrice);
-        _ticket.mint(msg.sender, false, 0, _ticketPrice, _ticketPrice);
-    }
-
-    function buyTicketMul(uint256 num) public override isAble {
+    function buyTicket(uint256 num) public override isAble {
         require(num > 0, "Invalid num.");
+        require(_busd.balanceOf(msg.sender) >= _ticketPrice * num, "Do not have enough BUSD.");
         for(uint256 i; i < num; i++) {
-            require(_busd.balanceOf(msg.sender) >= _ticketPrice, "Do not have enough BUSD.");
             _busd.transferFrom(msg.sender, _squidGameFound, _ticketPrice);
             _ticket.mint(msg.sender, false, 0, _ticketPrice, _ticketPrice);
         }
