e957663fd3278c724aa6e890e7970e1a6f2e8a15
DongM91 <874901723@qq.com>
2021-12-09 16:04:55 +0800

    update 2.0 test

--- contracts/ticketV1/interface/IOpenBox.sol
+++ contracts/ticketV1/interface/IOpenBox.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.0;
 
 interface IOpenBox {
+    event Claim(address indexed account);
+    event BuyTicket(address indexed account, uint256 num);
+
+
     function claim() external;
     function buyTicket(uint256 num) external;
 }
