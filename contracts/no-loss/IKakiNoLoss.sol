9ae55f51f1235fda8ae3a44c4499974c53a2f64f
liujx77777 <89775922+liujx77777@users.noreply.github.com>
2021-12-16 16:19:17 +0800

    修改和添加注释

--- contracts/no-loss/IKakiNoLoss.sol
+++ contracts/no-loss/IKakiNoLoss.sol
@@ -6,5 +6,5 @@ interface IKakiNoLoss {
     event AddStake(address indexed account, uint256 factionId,uint256 id,uint256 amount);
     event LeaveFaction(address indexed account,uint256 factionId,uint256 bonus);
     event Fire(address indexed account,uint256 chapter, uint256 lastRound, uint256 factionId,uint256 amount,bool binary, uint256 time);
-    event ClaimBonus(address indexed account,uint256 bonus);
+    event ClaimBonus(address indexed account,uint256 bonus,uint256 time);
 }
