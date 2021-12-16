bb24547f8f41291a8ac07024ed6b4a0614e0d977
kaki007 <94599324+metacod@users.noreply.github.com>
2021-12-16 11:51:55 +0800

    prepare token for user
    Signed-off-by: kaki007 <94599324+metacod@users.noreply.github.com>

--- test/noLoss.test.ts
+++ test/noLoss.test.ts
@@ -20,29 +20,33 @@ const setup = deployments.createFixture(async () => {
 
 describe('noloss game', async () => {
   it('single user full flow', async () => {
-    const { users, noLoss,usdt2, usdt, chainlink } = await setup();
+    const { users, noLoss, usdt, chainlink, kakiBnbLP, kakiUsdtLp, kakiToken, wbnbToken } = await setup();
     await users[0].usdt.approve(noLoss.address, parseEther(`1000000`));
     await usdt.transfer(users[0].address, parseEther('10000'));
 
-    const now = await noLoss.getTimestamp();
+    const now = await (await noLoss.getTimestamp()).toNumber();
 
     for (const u of users) {
-     
+      for (const t of [kakiToken, wbnbToken, kakiBnbLP, kakiUsdtLp]) {
+        await t.transfer(u.address, parseEther('10000'));
+        await t.connect(await ethers.getSigner(u.address)).approve(noLoss.address, parseEther('10000'));
+      }
     }
-    let factionId=await noLoss._nextFactionId();
-    console.log('factionId**************',factionId);
+
+    let factionId = await noLoss._nextFactionId();
+    console.log('factionId**************', factionId);
     await noLoss.createFaction(1);
-    await network.provider.send("evm_increaseTime", [7 *24 *60 * 60]);
+    await network.provider.send("evm_increaseTime", [7 * 24 * 60 * 60]);
     const factionStatus = await noLoss._factionStatus(1);
     printEtherResult(factionStatus);
     printEtherResultArray(factionStatus);
-    const kc=await noLoss.getChapterKC(1);
-    console.log('kc:*********',kc);
+    const kc = await noLoss.getChapterKC(1);
+    console.log('kc:*********', kc);
 
-    console.log('factionId**************',factionId);
+    console.log('factionId**************', factionId);
     //await noLoss.fire();
 
-    
+
 
   });
 });
