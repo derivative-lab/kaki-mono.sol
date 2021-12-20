ea69ecf7fe54dd5e49e5d2bf8073630c0eef4336
DongM91 <874901723@qq.com>
2021-12-20 14:31:15 +0800

    captain

--- contracts/captain/KakiCaptain.sol
+++ contracts/captain/KakiCaptain.sol
@@ -4,30 +4,29 @@ import "./interface/IKakiCaptain.sol";
 import "../base/BaseERC721.sol";
 contract KakiCaptain is IKakiCaptain, BaseERC721 {
 
+    uint256 public lowMember;
+    uint256 public mediumMember;
+    uint256 public highMember;
+
+    uint256 public lowCombineRate;
+    uint256 public mediumCombineRate;
+    uint256 public highCombineRate;
     mapping(uint256 => TicketPara) _ticketPara;
 
     function initialize() public initializer{
         __BaseERC721_init("", "");
     }
 
-    //uint256 _captainType,
-    // uint256 _memberNum,
-    // uint256 _miningRate,
-    // uint256 _combineRate
-
     function mint(
         address _to
     ) external override restricted returns (uint256 tokenId) {
         tokenId = totalMinted();
-        _ticketPara[tokenId].captainType = _captainType;
-        _ticketPara[tokenId].memberNum = _memberNum;
-        _ticketPara[tokenId].miningRate = _miningRate;
-        _ticketPara[tokenId].combineRate = _combineRate;
         _mint(_to, tokenId);
         increaceTokenId();
     }
 
     function getTicketMessage(uint256 tokenId) external override view returns (TicketPara memory) {
+        
         return (_ticketPara[tokenId]);
     }
 }
