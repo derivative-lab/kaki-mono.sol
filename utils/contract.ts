8b1ded53d2f0180afe82096e36bea2e257550d2f
liujx77777 <89775922+liujx77777@users.noreply.github.com>
2021-12-21 16:06:59 +0800

    upgrade

--- utils/contract.ts
+++ utils/contract.ts
@@ -34,8 +34,8 @@ export const mutiContractAddrs = {
     squidGame: '0xC76255D7E26A3d634Bd2CD4Ac84374e36C9798a5',
   },
   bsctest: {
-    squidGame: '0x94c9f5732c77b77f53a7E25C37f53D8731f054b6',
-    squidShortGame: '0x8b256bbA54B3f630fb172C5A3e4400DcbDbB469F',
+    squidGame: '0x8b256bbA54B3f630fb172C5A3e4400DcbDbB469F',
+    squidShortGame: '0x94c9f5732c77b77f53a7E25C37f53D8731f054b6',
     busd: '0xE70b02A5Ae129F66687256b7a5e81cC871e347D7',
     oracle: '0x8137934cF53e9ca1B4e75919dacc0364693fa69A',
     blindBoxDrop: '0x26dEa25C01f43B2cf8Da3aCe68F0DD830a4399e5',
@@ -49,7 +49,8 @@ export const mutiContractAddrs = {
     busd: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
     oracle: '0x8f55C31C0951C04d471744Eeef6a0d5903588EFe',
     squidTicket: '0xeC386352ab845a30BDCdb358D743F66487C6dF3f',
-    squidAllowList: '0x43bd49e5ad1173874ca5eb714858ec2af08d6e87',
+    squidAllowList: '0x390B7384dc96a5728BB08D2e864549b2dee64549',
+    // squidAllowList: '0x43bd49e5ad1173874ca5eb714858ec2af08d6e87',
     squidOpenBox: '0x67bab7f7dcde65738ef3db51e4148df1e5108354',
   }
 };
