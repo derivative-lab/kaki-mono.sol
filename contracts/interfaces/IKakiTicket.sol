4f29cf7cad34a98e0bbeebe919a11776a8e02028
DongM91 <874901723@qq.com>
2021-12-23 15:50:28 +0800

    k & K

---
+++ contracts/interfaces/IKakiTicket.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+import "./IBaseERC721.sol";
+interface IKakiTicket is IBaseERC721 {
+
+    struct TicketPara {
+        uint256 chip;
+        uint256 prob;
+        uint256 price;
+        uint256 ticketType;
+    }
+
+    function mint(
+        address _to,
+        uint256 _chip,
+        uint256 _prob,
+        uint256 _price,
+        uint256 _ticketType
+    ) external returns (uint256 tokenId) ;
+
+    function getTicketInfo(uint256 tokenId) external view returns (TicketPara memory);
+}
