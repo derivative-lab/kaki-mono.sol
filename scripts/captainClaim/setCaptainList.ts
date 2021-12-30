6d9290ef057672726777dd16f4a808d356d97091
DongM91 <874901723@qq.com>
2021-12-30 19:42:17 +0800

    add scripts

--- scripts/captainClaim/setCaptainList.ts
+++ scripts/captainClaim/setCaptainList.ts
@@ -2,12 +2,19 @@ import { parseEther } from 'ethers/lib/utils';
 import { captainClaimContract } from '~/utils/contract';
 import { contractAddress } from "../../utils/contract";
 import {formatEther} from 'ethers/lib/utils'
+// import _ from "lodash";
+
+
 
 (async () => {
     const captain = await captainClaimContract();
-    for(var i = 0; i < 5; i++) {
-        const tx = await captain.setTokenIdList(1+404*i,404*(i+1));
-        console.log(tx.hash);
-    }
+    // const tokenIds = Array.from({length:2020}).map((e,i)=>i+1);
+    // const group = _.chunk(tokenIds,500);
+    const tx = await captain.setTokenIdList(1, 200,{gasLimit: 5000000});
+    console.log(tx.hash);
+    // for(var i = 0; i < 10; i++) {
+    //     const tx = await captain.setTokenIdList(1 + 202*i, 202 * (i+1));
+    //     console.log(tx.hash);
+    // }
     
 })();
