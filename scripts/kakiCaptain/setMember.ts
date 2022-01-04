b77e5c56bcede5e73eee83b7616ac79012473c96
DongM91 <874901723@qq.com>
2022-01-04 18:23:09 +0800

    fix bug

---
+++ scripts/kakiCaptain/setMember.ts
@@ -0,0 +1,10 @@
+import { parseEther } from 'ethers/lib/utils';
+import { kakiCaptainContract } from '~/utils/contract';
+import { contractAddress } from "../../utils/contract";
+import {formatEther} from 'ethers/lib/utils'
+
+(async () => {
+    const ticket = await kakiCaptainContract();
+    const tx = await ticket.setMember(10, 2, 5);
+    console.log(tx.hash);
+})();
