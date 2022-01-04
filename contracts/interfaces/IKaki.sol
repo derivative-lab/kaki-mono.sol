dab560e659b63c329fa5537262775ff030947c00
kaki007 <94599324+metacod@users.noreply.github.com>
2022-01-04 12:03:21 +0800

    kaki contract

--- contracts/interfaces/IKaki.sol
+++ contracts/interfaces/IKaki.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-interface IKaki is IERC20 {
+
+import {IERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-IERC20PermitUpgradeable.sol";
+// import {IERC20MetadataUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
+// import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+import {IERC20} from "../interfaces/IERC20.sol";
+interface IKaki is IERC20, IERC20PermitUpgradeable{
     function mint(address to, uint256 amount) external;
 }
---
+++ contracts/kaki/Kaki.sol
@@ -0,0 +1,33 @@
+import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {IKaki} from "../interfaces/IKaki.sol";
+
+contract Kaki is ERC20PermitUpgradeable, IKaki, AccessControlUpgradeable {
+    bytes32 public constant MINTER = keccak256("MINTER");
+    event Mint(address indexed mintTo, address minter, uint256 amount);
+    uint256 public constant MAX_SUPPLY = 210000000 ether;
+
+    function initialize()
+        public
+        // string memory name,
+        // string memory symbol,
+        // uint256 initializedSupply
+        initializer
+    {
+        __AccessControl_init();
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        __ERC20Permit_init("Kaki");
+        __ERC20_init("Kaki", "KAKI");
+        //    _setRoleAdmin(MINTER, DEFAULT_ADMIN_ROLE);
+    }
+
+    function mint(address to, uint256 amount) public override onlyRole(MINTER) {
+        require(totalSupply() + amount <= MAX_SUPPLY, "too much supply");
+        _mint(to, amount);
+        emit Mint(to, msg.sender, amount);
+    }
+
+    function version() public pure returns (uint256) {
+        return 0;
+    }
+}
