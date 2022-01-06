fe5b341a1f796427acfb91e2afd504e930af334d
DongM91 <874901723@qq.com>
2022-01-06 14:11:21 +0800

    update scripts

--- scripts/captainClaim/setFoundAdd.ts
+++ scripts/captainClaim/setFoundAdd.ts
@@ -4,7 +4,7 @@ import { contractAddress } from "../../utils/contract";
 import {formatEther} from 'ethers/lib/utils'
 
 (async () => {
-    const captain = await captainClaimContract();
-    const tx = await captain.setFoundAdd('0x8b52ab88df16f88c4ec885d89889c6decac7e221');
-    console.log(tx.hash);
+    // const captain = await captainClaimContract();
+    // const tx = await captain.setFoundAdd('0x8b52ab88df16f88c4ec885d89889c6decac7e221');
+    // console.log(tx.hash);
 })();
