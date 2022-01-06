9c5e7b012a6aa79509be7fb63793ccac54480b85
DongM91 <874901723@qq.com>
2022-01-06 17:52:26 +0800

    nft update

--- scripts/captainClaim/setCaptainList.ts
+++ scripts/captainClaim/setCaptainList.ts
@@ -10,7 +10,7 @@ import {formatEther} from 'ethers/lib/utils'
     const captain = await captainClaimContract();
     // const tokenIds = Array.from({length:2020}).map((e,i)=>i+1);
     // const group = _.chunk(tokenIds,500);
-    const tx = await captain.setTokenIdList(1, 200,{gasLimit: 5000000});
+    const tx = await captain.setTokenIdList(1801, 2020,{gasLimit: 5000000});
     console.log(tx.hash);
     // for(var i = 0; i < 10; i++) {
     //     const tx = await captain.setTokenIdList(1 + 202*i, 202 * (i+1));
