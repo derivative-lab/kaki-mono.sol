22a217651506f3b17b8f7c4f020cf90214225f12
kaki007 <94599324+metacod@users.noreply.github.com>
2022-01-07 14:23:56 +0800

    add bnb pool

--- scripts/farm/addPool_bnb.ts
+++ scripts/farm/addPool_bnb.ts
@@ -10,7 +10,17 @@ import { farmContract } from '~/utils/contract';
 
   // https://github.com/alpaca-finance/bsc-alpaca-contract/blob/main/.testnet.json
 
-  const tx = await farm.addPool(100, zeroAddress(), 330 * 10000, '0xf9d32C5E10Dd51511894b360e6bD39D7573450F9','0xf9d32c5e10dd51511894b360e6bd39d7573450f9','0xac2fefDaF83285EA016BE3f5f1fb039eb800F43D',1,true,"BNB-pool",{gasLimit: 1000000});
+  const tx = await farm.addPool(
+    100, // allocation point
+    '0xDfb1211E2694193df5765d54350e1145FD2404A1', //zeroAddress(), // tokenAddress
+    330 * 10000,  // price
+    '0xf9d32C5E10Dd51511894b360e6bD39D7573450F9', // vault
+    '0xf9d32c5e10dd51511894b360e6bd39d7573450f9', // ibBNB
+    '0xac2fefDaF83285EA016BE3f5f1fb039eb800F43D', // fairLaunch
+    1,  // fairLaunch pid
+    true, // native
+    "BNB-pool" // name
+  );
 
   console.log(tx.hash);
 
