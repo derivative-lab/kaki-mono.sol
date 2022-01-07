b60d687e5e8b4b1757dc9e817db7fa162bb85f59
DongM91 <874901723@qq.com>
2022-01-07 16:41:07 +0800

    update script

--- scripts/mysteryBox/adminMint.ts
+++ scripts/mysteryBox/adminMint.ts
@@ -5,14 +5,15 @@ import {formatEther} from 'ethers/lib/utils'
 
 (async () => {
     const mBox = await mysteryBoxContract();
-    for(var i =0; i< 10; i++) {
-        const tx = await mBox.mint("0x471C5FE7175EEdaD67F3551eDc475a613BA1aAd1");
-        const tx1 = await mBox.mint("0x62b293CF6170C76ea908689f2eb93eB21e3f5084");
-        const tx2 = await mBox.mint("0x7430b734205366542B59541bC1201C46E666175c");
-        const tx3 = await mBox.mint("0xd7dFC7e4249c40f9915E64b3D343FEC00BA525eC");
+    //for(var i =0; i< 10; i++) {
+        // const tx = await mBox.mint("0x471C5FE7175EEdaD67F3551eDc475a613BA1aAd1");
+        // const tx1 = await mBox.mint("0x62b293CF6170C76ea908689f2eb93eB21e3f5084");
+        // const tx2 = await mBox.mint("0x7430b734205366542B59541bC1201C46E666175c");
+        // const tx3 = await mBox.mint("0xd7dFC7e4249c40f9915E64b3D343FEC00BA525eC");
+    const tx = await mBox.batchMint("0x5fB6F6F7A4D9a848Cd422F6ef399C9A1A128e894", 10);
 
-        console.log(tx.hash);
-        console.log(i);
-    }
+    console.log(tx.hash);
+    //console.log(i);
+    //}
     
 })();
