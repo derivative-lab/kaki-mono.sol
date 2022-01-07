00705cfe50c8eb4c9096f57165c9884151228c8e
DongM91 <874901723@qq.com>
2022-01-07 11:19:26 +0800

    update script

--- scripts/mysteryBox/adminMint.ts
+++ scripts/mysteryBox/adminMint.ts
@@ -6,7 +6,11 @@ import {formatEther} from 'ethers/lib/utils'
 (async () => {
     const mBox = await mysteryBoxContract();
     for(var i =0; i< 10; i++) {
-        const tx = await mBox.mint("0x62b293CF6170C76ea908689f2eb93eB21e3f5084");
+        const tx = await mBox.mint("0x471C5FE7175EEdaD67F3551eDc475a613BA1aAd1");
+        const tx1 = await mBox.mint("0x62b293CF6170C76ea908689f2eb93eB21e3f5084");
+        const tx2 = await mBox.mint("0x7430b734205366542B59541bC1201C46E666175c");
+        const tx3 = await mBox.mint("0xd7dFC7e4249c40f9915E64b3D343FEC00BA525eC");
+
         console.log(tx.hash);
         console.log(i);
     }
