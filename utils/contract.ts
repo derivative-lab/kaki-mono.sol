d52b0a1fc51f0c6282c242facdf405e8d58686f9
liujx77777 <89775922+liujx77777@users.noreply.github.com>
2022-01-07 19:45:05 +0800

    noloss

--- utils/contract.ts
+++ utils/contract.ts
@@ -2,7 +2,7 @@ import { ChainlinkRandoms } from './../typechain/ChainlinkRandoms.d';
 import { CaptainClaim__factory } from './../typechain/factories/CaptainClaim__factory';
 import { BlindBox, KakiCaptain__factory, KakiGarden__factory, KakiTicket, MysteryBox__factory } from './../typechain';
 import { deployments, ethers, network } from 'hardhat';
-import { AddressList__factory, MockChainLink__factory, MockToken__factory, Ticket__factory, OpenBox__factory, KakiSquidGame, KakiSquidGame__factory, BlindBox__factory, KakiTicket__factory, ERC20__factory, Kaki__factory, ClaimLock__factory } from '~/typechain';
+import { AddressList__factory, MockChainLink__factory, MockToken__factory, Ticket__factory, OpenBox__factory, KakiSquidGame, KakiSquidGame__factory, BlindBox__factory, KakiTicket__factory, ERC20__factory, Kaki__factory, ClaimLock__factory,KakiNoLoss__factory } from '~/typechain';
 
 
 export const frontendUsedContracts = [
@@ -213,4 +213,9 @@ export async function claimLockContract(signerIndex = 0) {
 
 export async function kakiContract(signerIndex = 0) {
   return Kaki__factory.connect(contractAddress.kaki, await getSigner(signerIndex));
+}
+
+
+export async function noLossContract(signerIndex = 0) {
+  return KakiNoLoss__factory.connect(contractAddress.noLoss, await getSigner(signerIndex));
 }
