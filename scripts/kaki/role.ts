f9f05f116d6f19ba954b01488cc31a4de3f6aa86
DongM91 <874901723@qq.com>
2022-01-10 19:11:28 +0800

    update unit test claimLock

--- scripts/kaki/role.ts
+++ scripts/kaki/role.ts
@@ -1,9 +1,10 @@
+import { contractAddress } from './../../utils/contract';
 import { kakiContract, getSigner } from '~/utils/contract';
 
 (async () => {
   const kaki = await kakiContract();
   const mintRole = await kaki.MINTER();
   const signer0 = await getSigner(0);
-  const tx = await kaki.grantRole(mintRole, signer0.address);
+  const tx = await kaki.grantRole(mintRole, contractAddress.claimLock);
   console.log(tx.hash);
 })();
