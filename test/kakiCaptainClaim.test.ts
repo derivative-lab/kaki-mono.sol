a3120cd9c5310c0c73a0a357161c87f5ef0d2708
DongM91 <874901723@qq.com>
2022-01-11 16:19:36 +0800

    update script

--- test/kakiCaptainClaim.test.ts
+++ test/kakiCaptainClaim.test.ts
@@ -22,32 +22,32 @@ return {
 };
 });
 
-describe('claim', async () => {
-    context("should success", async() => {
-      it('setList', async () => {
-        const { users, captainClaim, mysteryBox, kakiCaptain} = await setup();
-        await captainClaim.setTokenIdList(1, 100);
+// describe('claim', async () => {
+//     context("should success", async() => {
+//       it('setList', async () => {
+//         const { users, captainClaim, mysteryBox, kakiCaptain} = await setup();
+//         await captainClaim.setTokenIdList(1, 100);
 
-        let a = await captainClaim.getList();
-        console.log(a);
+//         let a = await captainClaim.getList();
+//         console.log(a);
 
-        await mysteryBox.setApprovalForAll(captainClaim.address, true);
-        await kakiCaptain.setupAdmin(captainClaim.address);
-        await mysteryBox.mint(users[0].address);
-        await mysteryBox.mint(users[0].address);
-        await mysteryBox.mint(users[0].address);
-        await mysteryBox.mint(users[0].address);
+//         await mysteryBox.setApprovalForAll(captainClaim.address, true);
+//         await kakiCaptain.setupAdmin(captainClaim.address);
+//         await mysteryBox.mint(users[0].address);
+//         await mysteryBox.mint(users[0].address);
+//         await mysteryBox.mint(users[0].address);
+//         await mysteryBox.mint(users[0].address);
 
-        //let tokenId = await captainClaim.mint({value: BigNumber.from("500000000000000000")});
-        //console.log(tokenId);
-        let balance = await kakiCaptain.balanceOf(users[0].address);
-        //expect (balance).to.equal(1);
-        await mysteryBox.setApprovalForAll(captainClaim.address, true);
-        for (var i = 0; i < 4; i ++) {
-            let boxId = await mysteryBox.tokenOfOwnerByIndex(users[0].address, i);
-            console.log("boxid", boxId);
-        }
-        await captainClaim.switchByBox(0);
-        });
-    });
-});
+//         //let tokenId = await captainClaim.mint({value: BigNumber.from("500000000000000000")});
+//         //console.log(tokenId);
+//         let balance = await kakiCaptain.balanceOf(users[0].address);
+//         //expect (balance).to.equal(1);
+//         await mysteryBox.setApprovalForAll(captainClaim.address, true);
+//         for (var i = 0; i < 4; i ++) {
+//             let boxId = await mysteryBox.tokenOfOwnerByIndex(users[0].address, i);
+//             console.log("boxid", boxId);
+//         }
+//         await captainClaim.switchByBox(0);
+//         });
+//     });
+// });
