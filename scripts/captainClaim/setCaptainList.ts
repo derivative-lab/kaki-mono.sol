ef3dfa74307c43086e5cd0719ef2cc06fe1cda51
liujx77777 <89775922+liujx77777@users.noreply.github.com>
2022-01-12 11:40:03 +0800

    noloss

--- scripts/captainClaim/setCaptainList.ts
+++ scripts/captainClaim/setCaptainList.ts
@@ -7,13 +7,15 @@ import {formatEther} from 'ethers/lib/utils'
 
 
 (async () => {
-    const captain = await captainClaimContract();
+    console.log('setidList init');
+    //const captain = await captainClaimContract();
     // const tokenIds = Array.from({length:2020}).map((e,i)=>i+1);
     // const group = _.chunk(tokenIds,500);
     for (var i =0;i<10; i++){
-        const tx = await captain.setTokenIdList(1+200*i, 200*(1+i),{gasLimit: 5000000});
-        console.log('i:',i,tx.hash);
+        //const tx = await captain.setTokenIdList(1+200*i, 200*(1+i),{gasLimit: 5000000});
+        //console.log('i:',i,tx.hash);
+        console.log('i:',1+200*i,200*(1+i));
     }
-    const tx = await captain.setTokenIdList(2001, 2020);
-    console.log(tx.hash);
+    //const tx = await captain.setTokenIdList(2001, 2020);
+    //console.log(tx.hash);
 })();
