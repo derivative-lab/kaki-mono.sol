231929aaf392382e6fc056e9cefb7dc7cbbebb8d
kaki007 <94599324+metacod@users.noreply.github.com>
2022-01-12 14:14:32 +0800

    deploy tools

--- scripts/farm/alpaca/vault/bnbVaultWithdraw.ts
+++ scripts/farm/alpaca/vault/bnbVaultWithdraw.ts
@@ -1,16 +1,16 @@
 
 
-import { IVault__factory, IVault, IERC20, IERC20__factory } from '~/typechain'
+import { IVault__factory, IVault, IERC20, IERC20__factory, MockValt__factory } from '~/typechain'
 import { connect } from 'http2';
 import { getSigner } from '~/utils/contract';
-import { parseEther } from 'ethers/lib/utils';
+import { parseEther, formatEther } from 'ethers/lib/utils';
 
 
 (async () => {
 
   const signer = await getSigner(0);
 
-  const vault = IVault__factory.connect("0xf9d32C5E10Dd51511894b360e6bD39D7573450F9", signer);
+  const vault = MockValt__factory.connect("0xf9d32C5E10Dd51511894b360e6bD39D7573450F9", signer);
 
   const ibBnb = IERC20__factory.connect("0xf9d32C5E10Dd51511894b360e6bD39D7573450F9", signer);
   console.log(await ibBnb.name());
@@ -18,10 +18,12 @@ import { parseEther } from 'ethers/lib/utils';
   console.log(await ibBnb.totalSupply());
   console.log(await ibBnb.decimals());
 
-
-  const v = parseEther('0.001');
-  const tx2 = await vault.withdraw(v);
-
+  const totalSupply = await vault.totalSupply();
+  const totalToken = await vault.totalToken();
+  const v = parseEther('0.1');
+  const ibv = v.mul(totalSupply).div(totalToken)
+  console.log(formatEther(ibv));
+  const tx2 = await vault.withdraw(ibv);
   console.log(tx2.hash)
 }
 )();
