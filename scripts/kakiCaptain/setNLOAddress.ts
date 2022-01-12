203bc86328cc4ea7e075956b8390c5c1ed0ac5c7
liujx77777 <89775922+liujx77777@users.noreply.github.com>
2022-01-12 17:06:38 +0800

    noloss

--- scripts/kakiCaptain/setNLOAddress.ts
+++ scripts/kakiCaptain/setNLOAddress.ts
@@ -5,10 +5,8 @@ import {formatEther} from 'ethers/lib/utils'
 
 (async () => {
     const caption = await kakiCaptainContract();
-    const adress1=await caption.nloAddress();
-    console.log(adress1);
-    //const tx = await caption.setNLOAddress("0x4d7541352aEea5cF34785e493BD7F9D2Ae58517A");
-    //console.log(tx.hash);
-    //const adress2=await caption.nloAddress();
-    //console.log(adress2);
+    const tx = await caption.setNLOAddress("0xc81CCa62FDF3C48EF483CE965CBe45673aF5C49b");
+    console.log(tx.hash);
+    const adress2=await caption.nloAddress();
+    console.log(adress2);
 })();
