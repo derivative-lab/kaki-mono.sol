790e92a208a1ca15c386e0d59686ad1809bdd77a
kaki007 <94599324+metacod@users.noreply.github.com>
2022-01-12 16:25:48 +0800

    fix farm withdraw

--- scripts/tools/depositAndWithdraw.ts
+++ scripts/tools/depositAndWithdraw.ts
@@ -4,7 +4,9 @@ import { parseEther } from 'ethers/lib/utils';
 (async () => {
   const tools = await toolsContract();
   const vault = '0xf9d32C5E10Dd51511894b360e6bD39D7573450F9'
-  const tx = await tools.depositAndWithdraw(vault, parseEther('0.1'), true, {gasLimit: 1000000});
+
+  const value = parseEther('0.1')
+  const tx = await tools.depositAndWithdraw(vault, value, true, { value, gasLimit: 1000000 });
   console.log(tx.hash);
 
   //const tx = await tools.depositAndWithdraw(vault, parseEther('0.1'), false)
