03b9aee796629827a4ab1d2c41e1fa3e89377c8c
kaki007 <94599324+metacod@users.noreply.github.com>
2022-01-13 19:43:47 +0800

    fix empoty staking harvest  bug

--- scripts/farm/harvest.ts
+++ scripts/farm/harvest.ts
@@ -1,10 +1,25 @@
-import { farmContract } from '~/utils/contract';
+import { farmContract, getSigner } from '~/utils/contract';
 import { parseEther } from 'ethers/lib/utils';
+import { printEtherResult } from '../../utils/logutil';
+import delay from 'delay';
 
 
 (async () => {
-
   const farm = await farmContract();
-  const tx = await farm.harvest(0);
+  const signer = await getSigner(1);
+  const farm1 = farm.connect(signer);
+  const user = await farm1._userInfo(0, signer.address);
+  printEtherResult(user);
+
+  const pending = await farm1.pendingReward(0)
+
+  printEtherResult(pending, 'pending');
+  const tx = await farm1.harvest(0);
   console.log(tx.hash)
+  await tx.wait();
+
+  await delay(5000);
+  const user1 = await farm1._userInfo(0, signer.address);
+  printEtherResult(user1);
+
 })();
